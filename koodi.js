ea65ef573b41f9d5e87a46dbc1cb4c0cc996def4
jhakulinen <hakulisenjukka96@gmail.com>
2020-11-26 23:12:15 +0200

    Lisätty kommentit

--- koodi.js
+++ koodi.js
@@ -4,24 +4,35 @@
 //Koodi näyttää yksinkertaiselta, mutta yhteensä projektiin kului aikaa noin 30h, koska olemme paskoja :D//
 
 //Luodaan funktio//
-function omena(){ 
+function omena(){
+//Tehdään haku// 
 var xmlhttp = new XMLHttpRequest();
-
+//Haetaan Espoon Omenan Finnkinon XML-tiedosto//
 xmlhttp.open("GET", "https://www.finnkino.fi/xml/Schedule/?area=1039", true);
 xmlhttp.send();
+//Aloitetaan vastauksen parsiminen//
 xmlhttp.onreadystatechange = function() {
 if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-
+    //Tallennetaan haettu XML-tiedosto muuttujaan xmlDoc//
     var xmlDoc = xmlhttp.responseXML;
+    //Luodaan muuttuja "table", johon lisätään sivulla näkyvän taulukon "otsikkorivi"//
     var table = "<tr><th></th><th>Aika</th><th>Elokuva</th><th>Teatteri</th><th>Sali</th><th>Kesto (min)</th></tr>";
+    //Luodaan muuttuja "x", johon lisätään haetusta XML-tiedostosta kaikki "Show" tagit. Jokainen Show tagi sisältää yhden elokuvan tiedot.//
     var x = xmlDoc.getElementsByTagName("Show")
+    //Luodaan muuttuja "elokuva", johon lisätään haetusta XML-tiedostosta kaikki "Title" tagit. "Title" tagi sisältää elokuvan nimen.// 
     var elokuva = xmlDoc.getElementsByTagName("Title");
+    //Luodaan muuttuja "teatteri", johon lisätään haetusta XML-tiedostosta kaikki "Theatre" tagit. Theatre tagi sisältää elokuvateatterin nimen.//
     var teatteri = xmlDoc.getElementsByTagName("Theatre");
+    //Luodaan muuttuja "sali", johon lisätään haetusta XML-tiedostosta kaikki "TheatreAuditorium" tagit. TheatreAuditorium tagi sisältää elokuvateatterin salin numeron ja/tai nimen//
     var sali = xmlDoc.getElementsByTagName("TheatreAuditorium");
+    //Luodaan muuttuja "pituus", johon lisätään haetusta XML-tiedostosta kaikki "LengthInMinutes" tagit. LengthInMinutes tagi sisältää elokuvan pituuden minuutteina//
     var pituus = xmlDoc.getElementsByTagName("LengthInMinutes");
+    //Luodaan muuttuja "aika", johon lisätään haetusta XML-tiedostosta kaikki "dttmShowStart" tagit. dttmShowStart tagi sisältää elokuvan alkamisajankohdan.//
     var aika = xmlDoc.getElementsByTagName("dttmShowStart");
+    //Luodaan muuttuja "kuva", johon lisätään haetusta XML-tiedostosta kaikki "EventSmallImagePortrait" tagit. EventSmallImagePortrait tagi sisältää elokuvan kansikuvan URL-osoitteen.//
     var kuva = xmlDoc.getElementsByTagName("EventSmallImagePortrait");
-    
+    //Luodaan for-silmukka, joka pyörii niin kauan, että jokaisen elokuvan tiedot on lisätty. Silmukka lisää yhden elokuvan tiedot "table" muuttujaan siten,//
+    //että yhden elokuvan tiedot ovat samalla rivillä, jonka jälkeen luodaan uusi rivi.//  
     for (i=0; i<x.length; i++) {
       var a = aika[i].innerHTML;
       table += "<tr><td>" +
@@ -44,6 +55,7 @@ if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
         
         
     }
+    //Lisää "table" muuttujaan for-silmukalla lisätyt tiedot "tabledata" nimiseen tyhjään taulukkoon.//
     document.getElementById("tabledata").innerHTML = table;
 }
 }
@@ -333,12 +345,14 @@ function itis(){
                 }
               
               
-
+//Luodaan funktio, joka päivittää sivun.//
 function reload(){
   location.reload();
 }
-
+//Haetaan button, jonka id on "index"(Etusivu). Lisätään EventListener, joka ajaa funktion "reload", kun "index" nappia painetaan.
 document.getElementById("index").addEventListener("click", reload)
+//Haetaan button, jonka id on "omena". Lisätään EventListener, joka ajaa funktion "omena", kun "omena" nappia painetaan.(Eli ajetaan Espoon Omenan Finnkinon tiedot hakeva ja taulukkoon lisäävä funktio silloin kun sivulla painetaan siihen viittaavaa nappia.)//
+//Sama periaate muidenkin buttoneiden osalta.//
 document.getElementById("omena").addEventListener("click", omena)
 document.getElementById("itis").addEventListener("click", itis)
 document.getElementById("sello").addEventListener("click", sello)
